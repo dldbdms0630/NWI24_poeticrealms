e4c76b4764b8941551e8b9665888d04de98ce161
dldbdms0630 <dldbdms0630@gmail.com>
2024-07-24 14:32:43 -0400

    poem handler script fixed and camera placement free spirits

--- Assets/SCRIPTS/ChangeTextOnTrigger.cs
+++ Assets/SCRIPTS/ChangeTextOnTrigger.cs
@@ -12,35 +12,48 @@ using TMPro;
 public class ChangeTextOnTrigger : MonoBehaviour
 {
     public TextMeshPro textObject;
-    public string newText = "New Text DUMMY";
+    // public TextMeshPro signalText; // for the "Press A to advance" message when player doesn't do anything for 3sec after the audio stops 
 
-    private InputData inputData; 
-    private bool isChanged = false, isGazed = false;
+    // public string newText = "New Text DUMMY";
+    public string[] newTexts = {"New Text A", "New Text B", "New Text C", "New Text D"};
+    public AudioClip[] audioClips; // Array of audio clips
 
+    private AudioSource audioSource; // Audio source to play the clips
+    private int textIdx = 0;
+
+    private InputData inputData;
+    private bool isStanzaDone = false, isGazed = false;
 
 
     private void Start()
     {
         inputData = GetComponent<InputData>();
-        Debug.Log("Started inputData: " + inputData);
-        // textObject = GameObject.Find("texts/poem1").GetComponent<TMP_Text>(); // added as bing rec 
+        audioSource = GetComponent<AudioSource>(); // Get the audio source component
 
+        // Debug.Log("Started inputData: " + inputData);
+        // textObject = GameObject.Find("texts/poem1").GetComponent<TMP_Text>(); // added as rec
     }
 
     void Update()
     {
         if (isGazed && inputData.rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Abutton))
         {
-            if (!isChanged && Abutton)
+            if (!isStanzaDone && Abutton && !audioSource.isPlaying)
             ChangeText();
         }
     }
 
     void ChangeText()
     {
-        textObject.text = newText;
-        isChanged = true;
+        if (textIdx < newTexts.Length) {
+            textObject.text = newTexts[textIdx];
+            audioSource.clip = audioClips[textIdx];
+            audioSource.Play();
+            textIdx++;
+        }
+        else isStanzaDone = true;
     }
+
 
     public void EnableGaze() 
     {
