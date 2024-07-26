495ea878db57b818c9cbe4619c7de56f0d8167c9
dldbdms0630 <dldbdms0630@gmail.com>
2024-07-26 01:19:05 -0400

    New message for the combined commit

--- Assets/SCRIPTS/ChangeTextOnTrigger.cs
+++ Assets/SCRIPTS/ChangeTextOnTrigger.cs
@@ -24,6 +24,7 @@ public class ChangeTextOnTrigger : MonoBehaviour
 
     private InputData inputData;
     private bool isStanzaDone = false, isGazed = false;
+    public bool canAdvanceText = true;
     private string sceneName;
 
 
@@ -36,19 +37,43 @@ public class ChangeTextOnTrigger : MonoBehaviour
 
     void Update()
     {
-        if (isGazed && inputData.rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Abutton))
+        if (isGazed && canAdvanceText && inputData.rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Abutton))
         {
             if (!isStanzaDone && Abutton && !audioSource.isPlaying)
-            ChangeText();
+            // ChangeText();
+            StartCoroutine(ChangeText());
         }
     }
 
-    void ChangeText()
+    // void ChangeText()
+    // {
+    //     if (textIdx < newTexts.Length) {
+    //         textObject.text = newTexts[textIdx];
+    //         audioSource.clip = audioClips[textIdx];
+    //         audioSource.Play();
+
+    //         if (sceneName == "Nurturers") {
+    //             if (newTexts[textIdx] == "Dreamed of you last night.")
+    //                 nurtObjManager.DisableTitle();
+                
+    //             else if (newTexts[textIdx] == "We ran away from robots—those brainsick bots—") 
+    //                 nurtObjManager.EnableTV();
+                
+                
+    //         }
+    //         textIdx++;
+    //     }
+    //     else isStanzaDone = true;
+    // }
+
+    IEnumerator ChangeText()
     {
         if (textIdx < newTexts.Length) {
             textObject.text = newTexts[textIdx];
             audioSource.clip = audioClips[textIdx];
             audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+
 
             if (sceneName == "Nurturers") {
                 if (newTexts[textIdx] == "Dreamed of you last night.")
@@ -57,6 +82,62 @@ public class ChangeTextOnTrigger : MonoBehaviour
                 else if (newTexts[textIdx] == "We ran away from robots—those brainsick bots—") 
                     nurtObjManager.EnableTV();
                 
+                else if (newTexts[textIdx] == "my mistake.") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnablePolaroidOne();
+                }
+                else if (newTexts[textIdx] == "your searching-fearless eyes, your") 
+                    nurtObjManager.EnablePolaroidTwo();
+
+                else if (newTexts[textIdx] == "seashells-don't-rust eyes.") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnableWindowPortal();
+                }
+
+                else if (newTexts[textIdx] == "Know")
+                    nurtObjManager.EnableWindowVideo();
+
+                else if (newTexts[textIdx] == "at 11:11, with raw faith, with silent love—") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnableHandPortal();
+                }
+
+                else if (newTexts[textIdx] == "notice: We're still running")
+                    nurtObjManager.EnableHandMovie();
+
+                else if (newTexts[textIdx] == "your breath is certain & we keep running.") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnableFirstWonder();
+                }
+                
+                else if (newTexts[textIdx] == "I wonder—")
+                    nurtObjManager.EnableParticles();
+
+                else if (newTexts[textIdx] == "you will slip away?") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnableComputerPortal();
+                }
+
+
+                else if (newTexts[textIdx] == "I wonder—you consoled me")
+                    // canAdvanceText = false;
+                    nurtObjManager.EnableComputerEvent();
+                
+
+                else if (newTexts[textIdx] == "where have you gone to?") {
+                    yield return new WaitForSeconds(1);
+                    nurtObjManager.EnableSecondToLast();
+                }
+
+
+                else if (newTexts[textIdx] == "So I surrender,")
+                    nurtObjManager.EnableSmileyStanza();
+
+                 else if (newTexts[textIdx] == "I-see-you-in-every-single-smiley—") {
+                    yield return new WaitForSeconds(3);
+                    nurtObjManager.GoToLastStanza();
+                }
+
             }
             textIdx++;
         }
--- Assets/SCRIPTS/NurtObjManager.cs
+++ Assets/SCRIPTS/NurtObjManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class NurtObjManager : MonoBehaviour
 {
@@ -8,11 +10,31 @@ public class NurtObjManager : MonoBehaviour
      * All public GameObjects are to be assigned in Inspector. These are all
      * pieced together by the poems! Each method is called with the corresponding line.
     */
+    // it just occurred to me that using tags is probably a better way BUT we have started already. 
+    
+    public GameObject frontSofaPortal;
+    public GameObject firstStanza;
+
     public GameObject tvScreen; 
     public GameObject firstLight;
     public Material topLightMaterial;
     public GameObject titleText;
-    // public GameObject frontSofaPortal;
+
+    public GameObject polaroidPortal;
+    public GameObject polaroidCamera;
+    public GameObject polaroidText;
+    public GameObject polaroidLight;
+    public GameObject polaroids;
+
+    public GameObject windowPortal;
+    public GameObject windowText;
+    public GameObject windowVideo;
+
+    public GameObject handPortal;
+    public GameObject handText;
+    public GameObject handLight;
+    public GameObject handVideo;
+
 
     public void DisableTitle()
     {
@@ -26,13 +48,106 @@ public class NurtObjManager : MonoBehaviour
         topLightMaterial.EnableKeyword("_EMISSION");
         topLightMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.None;
 
-
-//         mat.DisableKeyword("_EMISSION");
-// mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
-// mat.SetColor("_EmissionColor", Color.black);
-
-        
+        // mat.DisableKeyword("_EMISSION");
+        // mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
+        // mat.SetColor("_EmissionColor", Color.black);
     }
+
+    public void EnablePolaroidOne()
+    {
+        // set active just the polaroid portal and camera and text
+        frontSofaPortal.SetActive(false);
+        firstStanza.SetActive(false);
+        polaroidPortal.SetActive(true);
+        polaroidCamera.SetActive(true);
+        polaroidText.SetActive(true);
+    }
+
+    public void EnablePolaroidTwo()
+    {
+        // set active light in lamp, all of the 6 polaroids (just use parent), 
+        polaroids.SetActive(true);
+        polaroidLight.SetActive(true);
+        // frontSofaPortal.SetActive(false);
+        // firstStanza.SetActive(false);
+    }
+
+    public void EnableWindowPortal() 
+    {
+        polaroidPortal.SetActive(false);
+        polaroidText.SetActive(false);
+        windowPortal.SetActive(true);
+        windowText.SetActive(true);
+    }
+
+    public void EnableWindowVideo()
+    {
+        windowVideo.SetActive(true);
+    }
+
+    public void EnableHandPortal() 
+    {
+        windowPortal.SetActive(false);
+        windowText.SetActive(false);
+        handPortal.SetActive(true);
+        handText.SetActive(true);
+    }
+
+    public void EnableHandMovie()
+    {
+        handLight.SetActive(true);
+        handVideo.SetActive(true);
+
+    }
+
+    public void EnableFirstWonder()
+    {
+
+    }
+
+    public void EnableParticles()
+    {
+
+    }
+
+    public void EnableComputerPortal()
+    {
+
+    }
+
+    public void EnableComputerEvent()
+    {
+        FindObjectOfType<ChangeTextOnTrigger>().canAdvanceText = false;
+
+
+    }
+
+    public void OnComputerScreenTouched()
+    {
+        FindObjectOfType<ChangeTextOnTrigger>().canAdvanceText = true;
+    }
+
+    public void EnableSecondToLast()
+    {
+
+    }
+
+
+    public void EnableSmileyStanza()
+    {
+        // get back to portal that we have disabled 
+        // turn arc light on
+        // turn "I open my lips and" text on
+
+    }
+
+
+    public void GoToLastStanza()
+    {
+        SceneManager.LoadScene("NoFunNurt");
+    }
+
+
 
     // public void DisableObjects()
     // {
