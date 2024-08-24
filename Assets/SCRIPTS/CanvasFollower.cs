030d86aef1313dcbd67e441cd68ad38a840f91fa
donovan l <dlee124@terpmail.umd.edu>
2024-08-24 13:38:11 -0400

    schmovin text does NOT work
    we settle for regular subtitles

---
+++ Assets/SCRIPTS/CanvasFollower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class CanvasFollower : MonoBehaviour
+{
+    public Transform vrCamera; // Assign the VR camera here
+    public Vector3 offset = new Vector3(0, -0.3f, 1f); // Offset from the camera
+
+    void Update()
+    {
+        // Make the subtitle canvas follow the camera with an offset
+        transform.position = vrCamera.position + vrCamera.forward * offset.z + vrCamera.up * offset.y + vrCamera.right * offset.x;
+        transform.LookAt(vrCamera.position + vrCamera.forward * 10); // Make it always face the user
+    }
+}
